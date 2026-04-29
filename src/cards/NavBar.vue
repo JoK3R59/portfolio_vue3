12dce5304e62fbac5c0deb676b6ee329262aa8df
JoK3R59 <51001366+JoK3R59@users.noreply.github.com>
2026-04-29 11:37:05 +0200

    CHANGE style pour le responsive avec l'ajout d'un style xl:

--- src/cards/NavBar.vue
+++ src/cards/NavBar.vue
@@ -19,10 +19,10 @@ import { ref } from 'vue';
 
 <template>
   <div 
-    class="navbar fixed h-20 bottom-0 left-0 z-900 shadow-[0_2px_5px_rgba(0,0,0,0.1)] bg-[#0b1120] lg:bottom-auto lg:top-0 lg:shadow-[0_-3px_20px_rgba(156,156,156,0.5)]"
+    class="navbar fixed h-20 lg:h-14 xl:h-10 bottom-0 left-0 z-900 shadow-[0_2px_5px_rgba(0,0,0,0.1)] bg-[#0b1120] lg:bottom-auto lg:top-0 lg:shadow-[0_-3px_20px_rgba(156,156,156,0.5)]"
   >
     <div 
-      class="flex flex-row w-full max-w-screen justify-around items-center  lg:justify-between lg:px-8"
+      class="flex flex-row w-full max-w-screen justify-around items-center  lg:justify-between lg:px-4 xl:px-8"
     >
       <span class="hidden lg:block font-bold text-lg uppercase text-gray-50 tracking-wide">
         {{ props.name }}
@@ -30,7 +30,7 @@ import { ref } from 'vue';
       <a v-for="(value, id) in props.sectionTitles" :key="id"
         :href="id == 0 ? '#' : '#' + props.scrollTitles[id]"
         :class="activeId === id ? 'text-blue-400' : 'text-gray-50'"
-        class="flex flex-col items-center min-w-1/6 no-underline rounded-lg font-medium transition-all duration-[250ms] hover:text-blue-400 lg:flex-row lg:min-w-0 lg:py-4 lg:px-8 lg:hover:bg-white/10"
+        class="flex flex-col items-center min-w-1/6 no-underline rounded-lg font-medium transition-all duration-[250ms] hover:text-blue-400 lg:flex-row lg:min-w-36 xl:min-w-0 lg:py-2 xl:py-4 lg:px-4 xl:px-8 lg:hover:bg-white/10"
         @click="onLinkClick(id)"
       >
         <SvgConfig :name="value.icon" class="w-6 h-6 mb-1 lg:hidden lg:mb-0 lg:mr-2" />
